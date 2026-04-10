132e17d31a060aeb1d1f4db7663f29b84d15c629
sky690023980 <690023980@qq.com>
2026-04-10 23:47:55 +0800

    Add backend/plugins/__init__.py

---
+++ backend/plugins/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .base import BaziPlugin
+from .qimen_plugin import QimenPlugin
+from .ziwei_plugin import ZiweiPlugin
+from .health_plugin import HealthPlugin
+from .marriage_plugin import MarriagePlugin
+__all__ = [BaziPlugin, QimenPlugin, ZiweiPlugin, HealthPlugin, MarriagePlugin]
